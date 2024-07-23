2bedf6993d5816990d78d04fefd24e361f9d9d36
PaulEyyy122003 <curttutor@ckcm.edu.ph>
2024-07-23 09:46:37 +0800

    initial commit

---
+++ resources/views/product.blade.php
@@ -0,0 +1,3 @@
+<h1>this is product</h1>
+<h1>This product is for {{$name}} page</h1>
+
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about',function(){
+   // return('this is about page');
+  // return view('about');
+});
+Route::get('/product',function(){
+   // return('this is about product');
+   return view ('product', ['name' => 'shabu']);
+});
+Route::get('/name/{name?}',function($name='Good'){
+    return('My name is '. $name);
+});
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+});
